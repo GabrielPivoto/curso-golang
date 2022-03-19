dd33618a90599b9a941f443aba555282e8816fb3
Gabriel Pivoto <gabriel-pivoto@outlook.com>
2022-03-18 22:10:57 -0300

    First messages

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello Go")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50 //constant
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
+	fmt.Printf("We have %v tickets in total and there are %v tickets remaining\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
 }
